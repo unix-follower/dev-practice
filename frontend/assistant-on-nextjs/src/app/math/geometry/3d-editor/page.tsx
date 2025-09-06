bfbf80dbc50ad8ae69e3239d2fd0696628b1b81e
Artsem_Nikitsenka <artsem_nikitsenka@epam.com>
2025-09-06 14:05:57 +0300

    Add 3D geometry editor based on WebGL (assistant-on-nextjs app)

--- frontend/assistant-on-nextjs/src/app/math/geometry/3d-editor/page.tsx
+++ frontend/assistant-on-nextjs/src/app/math/geometry/3d-editor/page.tsx
@@ -1,14 +1,23 @@
 import React, { Suspense } from "react"
 import { getI18nDictionary } from "@/app/[lang]/dictionaries"
 import GeometryThreeJsEditor from "./_components/GeometryThreeJsEditor"
+import GeometryWebGLEditor from "./_components/GeometryWebGLEditor"
 
 export default async function Page({
   searchParams,
 }: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }) {
-  const { lang } = await searchParams
+  const { view, lang } = await searchParams
   const translations = await getI18nDictionary((lang as string) || "en")
+
+  if (view === "webgl") {
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <GeometryWebGLEditor translations={translations} />
+      </Suspense>
+    )
+  }
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
