6c4518d6530afad3581a64e1e6acf7bb30134ff3
Artsem_Nikitsenka <artsem_nikitsenka@epam.com>
2025-09-24 17:50:05 +0300

    Add translations to GeometryWebGLEditor (assistant-on-nextjs app)

--- frontend/assistant-on-nextjs/src/app/math/geometry/3d-editor/_components/GeometryWebGLEditor.tsx
+++ frontend/assistant-on-nextjs/src/app/math/geometry/3d-editor/_components/GeometryWebGLEditor.tsx
@@ -12,6 +12,214 @@ import smallRectangleVertexShader, { rectangleVertexShader } from "./shaders/rec
 import smallRectangleFragmentShader, { rectangleFragmentShader } from "./shaders/rectangleFrag"
 import triangleVertexShader from "./shaders/triangleVert"
 import triangleFragmentShader from "./shaders/triangleFrag"
+import { X_AXIS_INDEX, Y_AXIS_INDEX } from "@/lib/constants"
+
+interface SceneSettings {
+  gl: WebGL2RenderingContext
+  translateX?: number
+  translateY?: number
+}
+
+function clearCanvas(gl: WebGL2RenderingContext) {
+  gl.clearColor(0, 0, 0, 0)
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+}
+
+function computeMatrix({ gl, translateX = 200, translateY = 150 }: SceneSettings) {
+  let matrix = projection(gl.canvas.width, gl.canvas.height)
+  matrix = translate(matrix, translateX, translateY)
+  matrix = rotate(matrix, 0)
+  return scale(matrix, 1, 1)
+}
+
+function drawTriangles({ gl, offset = 0, count = 6 }: { gl: WebGL2RenderingContext; offset?: number; count?: number }) {
+  const primitiveType = gl.TRIANGLES
+  gl.drawArrays(primitiveType, offset, count)
+}
+
+function setTriangleGeometry(gl: WebGL2RenderingContext) {
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0, -100, 150, 125, -175, 100]), gl.STATIC_DRAW)
+}
+
+function setRectangleGeometry(gl: WebGL2RenderingContext) {
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([-150, -100, 150, -100, -150, 100, 150, -100, -150, 100, 150, 100]),
+    gl.STATIC_DRAW,
+  )
+}
+
+// function setRectangle(gl: WebGL2RenderingContext, x: number, y: number, width: number, height: number) {
+//   const x1 = x
+//   const x2 = x + width
+//   const y1 = y
+//   const y2 = y + height
+//   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]), gl.STATIC_DRAW)
+// }
+
+function setRandomRectangleColors(gl: WebGL2RenderingContext) {
+  // Pick 2 random colors.
+  const r1 = Math.random()
+  const b1 = Math.random()
+  const g1 = Math.random()
+  const r2 = Math.random()
+  const b2 = Math.random()
+  const g2 = Math.random()
+
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([r1, b1, g1, 1, r1, b1, g1, 1, r1, b1, g1, 1, r2, b2, g2, 1, r2, b2, g2, 1, r2, b2, g2, 1]),
+    gl.STATIC_DRAW,
+  )
+}
+
+function drawIsoscelesTriangle({ gl, translateX = 200, translateY = 150 }: SceneSettings) {
+  const program = webglUtils.createProgramFromSources(gl, [triangleVertexShader, triangleFragmentShader])!
+
+  // look up where the vertex data needs to go.
+  const positionLocation = gl.getAttribLocation(program, "a_position")
+
+  // lookup uniforms
+  const matrixLocation = gl.getUniformLocation(program, "u_matrix")
+
+  // Create set of attributes
+  const vao = gl.createVertexArray()
+  gl.bindVertexArray(vao)
+
+  const buffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+  setTriangleGeometry(gl)
+
+  // tell the position attribute how to pull data out of the current ARRAY_BUFFER
+  gl.enableVertexAttribArray(positionLocation)
+  const size = 2
+  const type = gl.FLOAT
+  const normalize = false
+  const stride = 0
+  const offset = 0
+  gl.vertexAttribPointer(positionLocation, size, type, normalize, stride, offset)
+
+  function drawScene(gl: WebGL2RenderingContext) {
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement)
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+    clearCanvas(gl)
+
+    // Tell it to use the pair of shaders
+    gl.useProgram(program)
+
+    // Bind the attribute/buffer set we want.
+    gl.bindVertexArray(vao)
+
+    const matrix = computeMatrix({ gl, translateX, translateY })
+    // Set the matrix.
+    gl.uniformMatrix3fv(matrixLocation, false, matrix)
+
+    drawTriangles({ gl, count: 3 })
+  }
+
+  drawScene(gl)
+}
+
+function drawSampleRectangle(gl: WebGL2RenderingContext) {
+  const program = webglUtils.createProgramFromSources(gl, [smallRectangleVertexShader, smallRectangleFragmentShader])!
+
+  const positionAttributeLocation = gl.getAttribLocation(program, "a_position")
+  const resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution")
+
+  const positionBuffer = gl.createBuffer()
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+
+  const positions = [10, 20, 80, 20, 10, 30, 10, 30, 80, 20, 80, 30]
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+
+  const vertexArrayObject = gl.createVertexArray()
+  gl.bindVertexArray(vertexArrayObject)
+
+  gl.enableVertexAttribArray(positionAttributeLocation)
+
+  const size = 2 // 2 components per iteration
+  const type = gl.FLOAT // the data is 32bit floats
+  const normalize = false
+  const stride = 0 // 0 = move forward size * sizeof(type) each iteration to get the next position
+  const offset = 0 // start at the beginning of the buffer
+  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset)
+
+  webglUtils.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement)
+
+  // Tell WebGL how to convert from clip space to pixels
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+
+  clearCanvas(gl)
+
+  // Tell it to use our program (pair of shaders)
+  gl.useProgram(program)
+
+  gl.bindVertexArray(vertexArrayObject)
+
+  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height)
+
+  drawTriangles({ gl })
+
+  return { webglContext: gl, program }
+}
+
+function drawRectangle({ gl, translateX, translateY }: SceneSettings) {
+  const program = webglUtils.createProgramFromSources(gl, [rectangleVertexShader, rectangleFragmentShader])!
+
+  const positionLocation = gl.getAttribLocation(program, "a_position")
+  const colorLocation = gl.getAttribLocation(program, "a_color")
+
+  // lookup uniforms
+  const matrixLocation = gl.getUniformLocation(program, "u_matrix")!
+
+  // Create set of attributes
+  const vao = gl.createVertexArray()
+  gl.bindVertexArray(vao)
+
+  // Create a buffer for the positons.
+  let buffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+
+  setRectangleGeometry(gl)
+
+  // tell the position attribute how to pull data out of the current ARRAY_BUFFER
+  gl.enableVertexAttribArray(positionLocation)
+  let size = 2
+  const type = gl.FLOAT
+  const normalize = false
+  const stride = 0
+  const offset = 0
+  gl.vertexAttribPointer(positionLocation, size, type, normalize, stride, offset)
+
+  // Create a buffer for the colors.
+  buffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+  setRandomRectangleColors(gl)
+
+  // tell the color attribute how to pull data out of the current ARRAY_BUFFER
+  gl.enableVertexAttribArray(colorLocation)
+  size = 4
+  gl.vertexAttribPointer(colorLocation, size, type, normalize, stride, offset)
+
+  webglUtils.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement)
+
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+
+  clearCanvas(gl)
+
+  // Tell it to use our program (pair of shaders)
+  gl.useProgram(program)
+
+  // Bind the attribute/buffer set we want.
+  gl.bindVertexArray(vao)
+
+  const matrix = computeMatrix({ gl, translateX, translateY })
+  // Set the matrix.
+  gl.uniformMatrix3fv(matrixLocation, false, matrix)
+  drawTriangles({ gl })
+}
 
 interface GeometryWebGLEditorProps {
   translations: Record<string, string | Record<string, string>>
@@ -31,72 +239,13 @@ export default function GeometryWebGLEditor({ translations }: GeometryWebGLEdito
       return
     }
 
-    function clearCanvas(gl: WebGL2RenderingContext) {
-      gl.clearColor(0, 0, 0, 0)
-      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+    const gl = currentCanvasRef!.getContext("webgl2")
+    if (!gl) {
+      throw new Error("WebGL is not supported")
     }
+    setWebGLContext(gl)
 
-    function draw(gl: WebGL2RenderingContext) {
-      const primitiveType = gl.TRIANGLES
-      const offset = 0
-      const count = 6
-      gl.drawArrays(primitiveType, offset, count)
-    }
-
-    function main() {
-      const gl = currentCanvasRef!.getContext("webgl2")
-      if (!gl) {
-        return
-      }
-      setWebGLContext(gl)
-
-      const program = webglUtils.createProgramFromSources(gl, [
-        smallRectangleVertexShader,
-        smallRectangleFragmentShader,
-      ])!
-
-      const positionAttributeLocation = gl.getAttribLocation(program, "a_position")
-      const resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution")
-
-      const positionBuffer = gl.createBuffer()
-
-      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-
-      const positions = [10, 20, 80, 20, 10, 30, 10, 30, 80, 20, 80, 30]
-      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
-
-      const vertexArrayObject = gl.createVertexArray()
-      gl.bindVertexArray(vertexArrayObject)
-
-      gl.enableVertexAttribArray(positionAttributeLocation)
-
-      const size = 2 // 2 components per iteration
-      const type = gl.FLOAT // the data is 32bit floats
-      const normalize = false
-      const stride = 0 // 0 = move forward size * sizeof(type) each iteration to get the next position
-      const offset = 0 // start at the beginning of the buffer
-      gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset)
-
-      webglUtils.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement)
-
-      // Tell WebGL how to convert from clip space to pixels
-      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-
-      clearCanvas(gl)
-
-      // Tell it to use our program (pair of shaders)
-      gl.useProgram(program)
-
-      gl.bindVertexArray(vertexArrayObject)
-
-      gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height)
-
-      draw(gl)
-
-      return { webglContext: gl, program }
-    }
-
-    const webglProgram = main()
+    const webglProgram = drawSampleRectangle(gl)
 
     return () => {
       if (webglProgram) {
@@ -104,7 +253,7 @@ export default function GeometryWebGLEditor({ translations }: GeometryWebGLEdito
         webglContext.deleteProgram(program)
       }
     }
-  })
+  }, [setWebGLContext])
 
   return (
     <WebGLRenderingCtx value={webGLContext}>
@@ -118,196 +267,48 @@ export default function GeometryWebGLEditor({ translations }: GeometryWebGLEdito
 
 function ToolPanel() {
   const { t } = useTranslation()
+  const [geometricTranslation, setGeometricTranslation] = useState([0, 0])
+  const [sceneRenderFn, setSceneRenderFn] = useState<((settings: SceneSettings) => void) | null>()
   const gl = useWebGLRenderingCtx()
+
+  useEffect(() => {
+    if (sceneRenderFn && gl) {
+      sceneRenderFn({
+        gl,
+        translateX: geometricTranslation[X_AXIS_INDEX],
+        translateY: geometricTranslation[Y_AXIS_INDEX],
+      })
+    }
+  }, [sceneRenderFn, gl, geometricTranslation])
 
   function handleDrawIsoscelesTriangleClick() {
     if (!gl) {
       return
     }
-
-    function drawIsoscelesTriangle(gl: WebGL2RenderingContext) {
-      const program = webglUtils.createProgramFromSources(gl, [triangleVertexShader, triangleFragmentShader])!
-
-      // look up where the vertex data needs to go.
-      const positionLocation = gl.getAttribLocation(program, "a_position")
-
-      // lookup uniforms
-      const matrixLocation = gl.getUniformLocation(program, "u_matrix")
-
-      // Create set of attributes
-      const vao = gl.createVertexArray()
-      gl.bindVertexArray(vao)
-
-      // Create a buffer.
-      const buffer = gl.createBuffer()
-      gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
-
-      // Set Geometry.
-      setGeometry(gl)
-
-      // tell the position attribute how to pull data out of the current ARRAY_BUFFER
-      gl.enableVertexAttribArray(positionLocation)
-      const size = 2
-      const type = gl.FLOAT
-      const normalize = false
-      const stride = 0
-      const offset = 0
-      gl.vertexAttribPointer(positionLocation, size, type, normalize, stride, offset)
-
-      function computeMatrix(gl: WebGL2RenderingContext) {
-        let matrix = projection(gl.canvas.width, gl.canvas.height)
-        matrix = translate(matrix, 200, 150)
-        matrix = rotate(matrix, 0)
-        return scale(matrix, 1, 1)
-      }
-
-      function drawScene(gl: WebGL2RenderingContext) {
-        webglUtils.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement)
-
-        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-
-        // Clear the canvas
-        gl.clearColor(0, 0, 0, 0)
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-        // Tell it to use the pair of shaders
-        gl.useProgram(program)
-
-        // Bind the attribute/buffer set we want.
-        gl.bindVertexArray(vao)
-
-        const matrix = computeMatrix(gl)
-        // Set the matrix.
-        gl.uniformMatrix3fv(matrixLocation, false, matrix)
-
-        // Draw the geometry.
-        const offset = 0
-        const count = 3
-        gl.drawArrays(gl.TRIANGLES, offset, count)
-      }
-
-      drawScene(gl)
-    }
-
-    // Fill the buffer with the values that define a triangle.
-    function setGeometry(gl: WebGL2RenderingContext) {
-      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0, -100, 150, 125, -175, 100]), gl.STATIC_DRAW)
-    }
-
-    drawIsoscelesTriangle(gl)
+    setSceneRenderFn(() => (settings: SceneSettings) => drawIsoscelesTriangle(settings))
   }
 
   function handleDrawRectangleClick() {
-    function main() {
-      if (!gl) {
-        return
-      }
-
-      // setup GLSL program
-      const program = webglUtils.createProgramFromSources(gl, [rectangleVertexShader, rectangleFragmentShader])!
-
-      // look up where the vertex data needs to go.
-      const positionLocation = gl.getAttribLocation(program, "a_position")
-      const colorLocation = gl.getAttribLocation(program, "a_color")
-
-      // lookup uniforms
-      const matrixLocation = gl.getUniformLocation(program, "u_matrix")
-
-      // Create set of attributes
-      const vao = gl.createVertexArray()
-      gl.bindVertexArray(vao)
-
-      // Create a buffer for the positons.
-      let buffer = gl.createBuffer()
-      gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
-
-      // Set Geometry.
-      setGeometry(gl)
-
-      // tell the position attribute how to pull data out of the current ARRAY_BUFFER
-      gl.enableVertexAttribArray(positionLocation)
-      let size = 2
-      const type = gl.FLOAT
-      const normalize = false
-      const stride = 0
-      const offset = 0
-      gl.vertexAttribPointer(positionLocation, size, type, normalize, stride, offset)
-
-      // Create a buffer for the colors.
-      buffer = gl.createBuffer()
-      gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
-      // Set the colors.
-      setColors(gl)
-
-      // tell the color attribute how to pull data out of the current ARRAY_BUFFER
-      gl.enableVertexAttribArray(colorLocation)
-      size = 4
-      gl.vertexAttribPointer(colorLocation, size, type, normalize, stride, offset)
-
-      function computeMatrix(gl: WebGL2RenderingContext) {
-        let matrix = projection(gl.canvas.width, gl.canvas.height)
-        matrix = translate(matrix, 200, 150)
-        matrix = rotate(matrix, 0)
-        return scale(matrix, 1, 1)
-      }
-
-      drawScene(gl)
-
-      // Draw the scene.
-      function drawScene(gl: WebGL2RenderingContext) {
-        webglUtils.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement)
-
-        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-
-        // Clear the canvas
-        gl.clearColor(0, 0, 0, 0)
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-        // Tell it to use our program (pair of shaders)
-        gl.useProgram(program)
-
-        // Bind the attribute/buffer set we want.
-        gl.bindVertexArray(vao)
-
-        const matrix = computeMatrix(gl)
-        // Set the matrix.
-        gl.uniformMatrix3fv(matrixLocation, false, matrix)
-
-        // Draw the geometry.
-        const offset = 0
-        const count = 6
-        gl.drawArrays(gl.TRIANGLES, offset, count)
-      }
+    if (!gl) {
+      return
     }
+    setSceneRenderFn(() => (settings: SceneSettings) => drawRectangle(settings))
+  }
 
-    // Fill the buffer with the values that define a rectangle.
-    function setGeometry(gl: WebGL2RenderingContext) {
-      gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array([-150, -100, 150, -100, -150, 100, 150, -100, -150, 100, 150, 100]),
-        gl.STATIC_DRAW,
-      )
-    }
+  function updatePosition(axisIndex: number, position: number) {
+    const translation = [...geometricTranslation]
+    translation[axisIndex] = position
+    setGeometricTranslation(translation)
+  }
 
-    // Fill the buffer with colors for the 2 triangles
-    // that make the rectangle.
-    function setColors(gl: WebGL2RenderingContext) {
-      // Pick 2 random colors.
-      const r1 = Math.random()
-      const b1 = Math.random()
-      const g1 = Math.random()
-      const r2 = Math.random()
-      const b2 = Math.random()
-      const g2 = Math.random()
+  function handleXPositionSliderInput(e: React.ChangeEvent<HTMLInputElement>) {
+    const x = Number.parseInt(e.target.value)
+    updatePosition(X_AXIS_INDEX, x)
+  }
 
-      gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array([r1, b1, g1, 1, r1, b1, g1, 1, r1, b1, g1, 1, r2, b2, g2, 1, r2, b2, g2, 1, r2, b2, g2, 1]),
-        gl.STATIC_DRAW,
-      )
-    }
-
-    main()
+  function handleYPositionSliderInput(e: React.ChangeEvent<HTMLInputElement>) {
+    const y = Number.parseInt(e.target.value)
+    updatePosition(Y_AXIS_INDEX, y)
   }
 
   return (
@@ -327,6 +328,37 @@ function ToolPanel() {
       </Tooltip>
       <div>
         <span>{t("geometry3DEditorPage.lights")}</span>
+      </div>
+      <div>
+        <span>Transformations</span>
+      </div>
+      <div className="slide-container">
+        <p>
+          <label htmlFor="x-position-slider">x</label>
+          <input
+            id="x-position-slider"
+            className="slider"
+            type="range"
+            min="0"
+            max={gl?.canvas.width}
+            step="1"
+            value={geometricTranslation[X_AXIS_INDEX]}
+            onInput={handleXPositionSliderInput}
+          />
+        </p>
+        <p>
+          <label htmlFor="y-position-slider">y</label>
+          <input
+            id="y-position-slider"
+            className="slider"
+            type="range"
+            min="0"
+            max={gl?.canvas.height}
+            step="1"
+            value={geometricTranslation[Y_AXIS_INDEX]}
+            onInput={handleYPositionSliderInput}
+          />
+        </p>
       </div>
     </div>
   )
