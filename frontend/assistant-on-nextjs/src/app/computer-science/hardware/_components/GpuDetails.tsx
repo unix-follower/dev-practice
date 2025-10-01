29c216d2b45839bac11123d618253a4c4b24658a
Artsem_Nikitsenka <artsem_nikitsenka@epam.com>
2025-10-01 21:05:07 +0300

    Add CSS style (assistant-on-nextjs app)

--- frontend/assistant-on-nextjs/src/app/computer-science/hardware/_components/GpuDetails.tsx
+++ frontend/assistant-on-nextjs/src/app/computer-science/hardware/_components/GpuDetails.tsx
@@ -3,16 +3,32 @@ import { useStyleEngine } from "@/lib/hooks/styleEngineHooks"
 import styles from "./gpuDetails.module.css"
 import { useTranslations } from "next-intl"
 
-interface GpuDetailsProps {
-  id: string
-  className?: string
-}
-
 const details = new Map()
 details.set(
   "NVIDIA_GB207-300",
   "NVIDIA's GB207 GPU uses the Blackwell 2.0 architecture and is made using a 5 nm production process at TSMC. With a die size of 149 mm² and a transistor count of 16,900 million it is a small chip. GB207 supports DirectX 12 Ultimate (Feature Level 12_2). For GPU compute applications, OpenCL version 3.0 and CUDA 12.0 can be used. Additionally, the DirectX 12 Ultimate capability guarantees support for hardware-raytracing, variable-rate shading and more, in upcoming video games. It features 2560 shading units, 80 texture mapping units and 32 ROPs. Also included are 80 tensor cores which help improve the speed of machine learning applications. The GPU also contains 20 raytracing acceleration cores.",
 )
+
+const twStyleMap = new Map()
+twStyleMap.set(
+  "tw-template1",
+  "line-clamp-1 align-super leading-none font-bold tracking-wide break-keep whitespace-pre text-current lowercase italic line-through decoration-sky-500",
+)
+twStyleMap.set(
+  "tw-template2",
+  "truncate text-right align-bottom font-sans text-sm leading-none font-extralight tracking-normal break-all text-inherit decoration-pink-500 decoration-solid decoration-1",
+)
+twStyleMap.set(
+  "tw-template3",
+  "line-clamp-none list-decimal border-stone-500 align-text-bottom leading-3 capitalize font-stretch-condensed underline-offset-1 bg-gray-700/70",
+)
+twStyleMap.set("tw-template4", "line-clamp-1 bg-blue-700 text-red-200")
+twStyleMap.set("tw-template5", "border-teal-950/30 bg-pink-400/[85.2%]")
+
+interface GpuDetailsProps {
+  id: string
+  className?: string
+}
 
 export default function GpuDetails({ id, className }: GpuDetailsProps) {
   const t = useTranslations("computerScienceHardwarePage")
@@ -21,6 +37,8 @@ export default function GpuDetails({ id, className }: GpuDetailsProps) {
   let style = className
   if (styleEngine === "css" && className) {
     style = styles[className]
+  } else if (styleEngine === "tw" && twStyleMap.has(className)) {
+    style = twStyleMap.get(className)
   }
 
   if (details.has(id)) {
